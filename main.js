295b0bd21635c339a7a5bb30af2c1028b426b134
Qabas al ani <doctorqa@yahoo.com>
2022-02-03 12:45:01 -0500

    created a map that will go through and get breeds

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 async function start() {
-    const response = await fetch("https://dog.ceo/api/breeds/list/all")
-    const data = await response.json()
-
+  const response = await fetch("https://dog.ceo/api/breeds/list/all");
+  const data = await response.json();
+  creatBreedList(data.message);
 }
 
-start()
+start();
 
-
+function creatBreedList(breedList) {
+  document.getElementById("breed").innerHTML = `
+  <select>
+        <option>Choose a dog</option>
+        ${Object.keys(breedList)
+          .map(function (breed) {
+            return `<option>${breed}</option>`;
+          })
+          .join("")}
+      </select>
+`;
+}
