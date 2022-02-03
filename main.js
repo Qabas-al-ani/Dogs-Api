d5abde5fce551df168a78960405adeacf494bc03
Qabas al ani <doctorqa@yahoo.com>
2022-02-03 13:02:48 -0500

    got the loaded back user choices function

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ start();
 
 function creatBreedList(breedList) {
   document.getElementById("breed").innerHTML = `
-  <select>
+  <select onchange="loadByBreed(this.value)">
         <option>Choose a dog</option>
         ${Object.keys(breedList)
           .map(function (breed) {
@@ -17,4 +17,12 @@ function creatBreedList(breedList) {
           .join("")}
       </select>
 `;
+}
+
+async function loadByBreed(breed) {
+  if (breed != "Choose a dog") {
+    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
+    const data = await response.json();
+    console.log(data);
+  }
 }
